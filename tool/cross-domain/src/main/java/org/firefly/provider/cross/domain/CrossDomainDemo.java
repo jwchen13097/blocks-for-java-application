2525529f7299c79dffd965a15a752db437501f3c
jwchen13097 <jwchen13097@yeah.net>
2020-03-16 10:55:14 +0800

    add example for cross domain.

---
+++ tool/cross-domain/src/main/java/org/firefly/provider/cross/domain/CrossDomainDemo.java
@@ -0,0 +1,11 @@
+package org.firefly.provider.cross.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossDomainDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(CrossDomainDemo.class, args);
+    }
+}
