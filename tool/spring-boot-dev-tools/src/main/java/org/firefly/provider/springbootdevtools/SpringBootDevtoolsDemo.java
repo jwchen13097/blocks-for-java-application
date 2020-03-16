b94f73f372b6c4fd62dd8e87800e3a1cdb3d3238
jwchen13097 <jwchen13097@yeah.net>
2020-03-16 11:05:52 +0800

    add hot deployment usage example.

---
+++ tool/spring-boot-dev-tools/src/main/java/org/firefly/provider/springbootdevtools/SpringBootDevtoolsDemo.java
@@ -0,0 +1,11 @@
+package org.firefly.provider.springbootdevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevtoolsDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsDemo.class, args);
+    }
+}
---
+++ tool/spring-boot-dev-tools/src/main/java/org/firefly/provider/springbootdevtools/rest/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.firefly.provider.springbootdevtools.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Welcome")
+public class HelloWorldController {
+    @GetMapping("/Greeting/{name}")
+    public String greet(@PathVariable String name) {
+        return "Welcome, " + name + "!";
+    }
+}
