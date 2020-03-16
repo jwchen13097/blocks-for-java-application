87ba03aff138e46295f440b4efa5f693eed85e4e
jwchen13097 <jwchen13097@yeah.net>
2020-03-16 10:51:11 +0800

    add checkstyle for code format check.

---
+++ tool/checkstyle/src/main/java/org/firefly/provider/checkstyle/CheckStyleDemo.java
@@ -0,0 +1,8 @@
+package org.firefly.provider.checkstyle;
+
+// 当前项目中执行命令即可进行checkstyle检查：mvn clean install -U
+public class CheckStyleDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
